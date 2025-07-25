56c7b7ebf3418951a3f7df3491c34586a6e45dd8
nerisa16 <salgadonerisa@gmail.com>
2025-07-25 16:34:02 +0800

    Update StudentModel.php

--- src/Models/StudentModel.php
+++ src/Models/StudentModel.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace Fat\Gs\Models;
-
-use Fat\Gs\Core\Crud;
-use  Fat\Gs\Core\Database;
+namespace Caraballo\Gs\Models;
+use Caraballo\Gs\Core\Crud;
+use Caraballo\Gs\Core\Database;
 
 class StudentModel extends Database implements Crud {
 
@@ -13,22 +12,24 @@ class StudentModel extends Database implements Crud {
     public int $year_level;
     public string $section;
 
-    public function __construct() 
+    public function __construct()
     {
         parent::__construct();
         $this->id = 0;
         $this->name = "";
-        $this->course = "";
+        $this->couse = "";
         $this->year_level = 0;
         $this->section = "";
     }
-    public function find($id) {
-
+    
+    public function create(){
+        $query = $this->conn->prepare("INSERT INTO `students`(`id`,`name`,`course`,`year_level`,`section`)
+        VALUES('$this->id','$this->name','$this->course','$this->year_level','$this->section')");
+        if($query->execute()) {
+            echo "Student inserted successfully!";
+        }
     }
-    public function create() {
-
-    }
-    public function read() {
+    public function read(){
         try {
             $sql = "SELECT * FROM students";
             $results = $this->conn->query($sql);
@@ -38,10 +39,25 @@ class StudentModel extends Database implements Crud {
         }
 
     }
-    public function update($id) {
+    public function update($id){
+        $this->id = $id;
+        $sql = "UPDATE students SET name = '$this->name', course = '$this->course', 
+        year_level = '$this->year_level', section = '$this->section' WHERE id = '$this->id'";
+        if($this->conn->query($sql)) {
+            echo "Student successfully updated!";
+        } else {
+            echo "Update failed:" .$this->conn->error;
+        }
 
     }
-    public function delete($id) {
+    public function delete($id){
+        $this->id = $id;
+        $sql = "DELETE FROM `students` WHERE id = $this->id";
+        if($this->conn->query($sql)) {
+            echo "Student successfully deleted!";
+        } else {
+            echo "Student delete failed!" .$this->conn->error;
+        }
         
     }
 
